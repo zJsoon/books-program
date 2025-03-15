e416e22851bc9f7d988447dfe739703113241d81
zJson <admin@zjson.dev>
2025-03-15 12:26:19 +0100

    EDIT - addLibro

--- app/src/books/book.c
+++ app/src/books/book.c
@@ -29,5 +29,22 @@ Book askBook(){
 }
 
 void addLibro(lBook *lb, Book b){
+	int i;
 
+	    if (lb->aBook == NULL) {
+	        lb->aBook = malloc(1 * sizeof(Book));
+	    } else {
+	        Book *aux = malloc(lb->numBook * sizeof(Book));
+	        for (i = 0; i < lb->numBook; i++) {
+	            aux[i] = lb->aBook[i];
+	        }
+	        free(lb->aBook);
+	        lb->aBook = malloc((lb->numBook + 1) * sizeof(Book));
+	        for (i = 0; i < lb->numBook; i++) {
+	            lb->aBook[i] = aux[i];
+	        }
+	        free(aux);
+	    }
+	    lb->aBook[lb->numBook] = b;
+	    lb->numBook++;
 }
